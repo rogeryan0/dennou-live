b4c03cb3e3cc50d915e8601f62aa64f16dbb59b1
Jun NOGATA <nogajun@gmail.com>
2008-10-30 16:55:56 +0900

    fix hook script

--- config/chroot_local-hooks/02-stop-daemon.sh
+++ config/chroot_local-hooks/02-stop-daemon.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
-#update-rc.d -f openbsd-inetd remove
-#update-rc.d -f stop-bootlogd remove
-#update-rc.d -f cron remove
-#update-rc.d -f syslogd remove
-#update-rc.d -f saned remove
-DAEMON="openbsd-inetd stop-bootlogd cron syslogd saned clamav-freshclam bluetooth rsync"
-for i in $DAEMON; do update-rc.d -f $i; done
+DAEMON="openbsd-inetd stop-bootlogd cron syslogd saned clamav-freshclam rsync"
+for i in ${DAEMON}; do /usr/sbin/update-rc.d -f ${i} remove; done
