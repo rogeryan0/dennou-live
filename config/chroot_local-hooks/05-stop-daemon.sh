c1982205644fbd3e6cbed175a89b47399e079a48
Jun NOGATA <nogajun@gmail.com>
2008-11-03 22:06:48 +0900

    remove insserv

--- config/chroot_local-hooks/05-stop-daemon.sh
+++ config/chroot_local-hooks/05-stop-daemon.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-DAEMON="openbsd-inetd stop-bootlogd cron saned clamav-freshclam rsync samba cups portmap rsyslog openvpn atd"
+DAEMON="openbsd-inetd stop-bootlogd cron saned clamav-freshclam rsync samba cups portmap rsyslog openvpn atd citadel binfmt-support"
 for i in $DAEMON; do /usr/sbin/update-rc.d -f $i remove; done
