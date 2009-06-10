baf48310cd63af3adf0962102aa60e311a6a6cda
Jun NOGATA <nogajun@gmail.com>
2009-06-10 18:10:03 +0900

    small fix

--- config/chroot_local-hooks/02-resolvconf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-# aptitude -y install resolvconf
-cd /etc
-rm resolv.conf resolv.conf.orig
-ln -s /etc/resolvconf/run/resolv.conf resolv.conf
-ln -s /etc/resolvconf/run/resolv.conf resolv.conf.orig
