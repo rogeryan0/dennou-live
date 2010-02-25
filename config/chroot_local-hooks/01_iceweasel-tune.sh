09c5f62eaf9560a9e48992084c65a876f22a9a63
Youhei SASAKI <uwabami@gfd-dennou.org>
2010-02-25 22:25:01 +0900

    misc changed for live-helper 2.0

--- config/chroot_local-hooks/01_iceweasel-tune.sh
+++ config/chroot_local-hooks/01_iceweasel-tune.sh
@@ -6,7 +6,6 @@ set -e
 ICEWEASEL_PREFS=/etc/iceweasel/profile/prefs.js
 
 cat << _EOL_ >>${ICEWEASEL_PREFS}
-
 /* Debian Live tune */
 user_pref("browser.cache.disk.parent_directory","/tmp");
 user_pref("browser.cache.disk.capacity", 5000);
