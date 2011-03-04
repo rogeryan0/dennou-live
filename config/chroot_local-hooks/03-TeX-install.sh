3c44766581c1c7282398d3afb513ffc5c113b587
Youhei SASAKI <uwabami@gfd-dennou.org>
2011-03-04 12:35:23 +0900

    Add tex/imagemagick, syslinux templates

---
+++ config/chroot_local-hooks/03-TeX-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -x
+set -e
+apt-get -y --no-install-recommends install ptex-bin texlive-latex-extra okumura-clsfiles ghostscript-x gs-cjk-resource xdvik-ja poppler-data cmap-adobe-japan1 cmap-adobe-japan2 dvipsk-ja dvipdfmx
+jisftconfig add
+update-vfontmap
+#apt-get -y install dennou-sty-6
--- config/chroot_local-hooks/03-TeX.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -x
-set -e
-jisftconfig add
-update-vfontmap
-apt-get -y install dennou-sty-6
